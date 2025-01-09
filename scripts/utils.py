ae6817f075768a79fcc4229b5275fc3c616e22b4
kurnik <c.jaworski@fu-berlin.de>
2025-01-09 01:23:54 +0100

    feat: add logging functions for email summary and success tracking

--- scripts/utils.py
+++ scripts/utils.py
@@ -18,3 +18,22 @@ def load_data():
         if os.path.isfile(os.path.join(attachments_folder, f))
     ]
     return recipients, attachments
+
+
+def log_email_summary(total_sent, len_recipients):
+    log_and_print(f"Successfully sent {total_sent}/{len_recipients} emails")
+
+
+def log_success(index, recipient, len_recipients, sent_attachments, success):
+    failed_recipients, successful_attachments, skipped_attachments = [], set(), set()
+
+    if success:
+        successful_attachments.update(str(a) for a in sent_attachments)
+        log_and_print(f"{index}/{len_recipients} Successfully sent email to {
+                      recipient['email']}")
+        logger.info(f"{index}/{len_recipients} Attachments sent to {recipient['email']}: {
+            [os.path.basename(a) for a in sent_attachments]}")
+    else:
+        failed_recipients.append(recipient['email'])
+        log_and_print(f"{index}/{len_recipients} Failed to send email to {
+                      recipient['email']}", "error")
